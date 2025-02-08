aa7503e463b9e922f10b0c2ff6ab19b3e1335d46
KevinStop <84851140+KevinStop@users.noreply.github.com>
2025-02-07 23:28:58 -0500

    init

---
+++ FrontEnd/.angular/cache/18.2.14/gestion_inventario/vite/deps/primeng_paginator.js
@@ -0,0 +1,25 @@
+import {
+  Paginator,
+  PaginatorClasses,
+  PaginatorModule,
+  PaginatorStyle
+} from "./chunk-K5KEVY3J.js";
+import "./chunk-YH2QYDP3.js";
+import "./chunk-QAT4YICN.js";
+import "./chunk-VPINC7MZ.js";
+import "./chunk-ELAHLNOL.js";
+import "./chunk-I3GN3EYL.js";
+import "./chunk-AZTBXGFO.js";
+import "./chunk-MIJT6FLR.js";
+import "./chunk-O4XABTPG.js";
+import "./chunk-4N4GOYJH.js";
+import "./chunk-5OPE3T2R.js";
+import "./chunk-FHTVLBLO.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  Paginator,
+  PaginatorClasses,
+  PaginatorModule,
+  PaginatorStyle
+};
+//# sourceMappingURL=primeng_paginator.js.map
